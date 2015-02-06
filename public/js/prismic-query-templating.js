4984b99e21788529ff3dd3efb6f7602adcecc161
RhysC <rhysevancampbell@gmail.com>
2015-02-06 22:14:10 +0800

    basic article display working, however is needs to be better structured - the asHtml() method is not going to be ideal

--- public/js/prismic-query-templating.js
+++ public/js/prismic-query-templating.js
@@ -1,8 +1,5 @@
 (function(global) {
-	//arguments to this function will be used as the predicates for the query
-	global.getResources = function(){
-		predicates = arguments;
-		var maps = {
+	var maps = {
 			person: function(ctx, item){
 				return {
 								name : item.getText("person.first_name") + " " + item.getText("person.surname"),
@@ -21,15 +18,31 @@
 								image : item.getImage("organisation.illustration").views.icon.url,
 								link : item.getLink("organisation.website").value.url
 							};
-			}
+			},
+			article : function(ctx, item){
+				return {
+								displayName : item.getText("organisation.displayName"),
+								about : item.getText("organisation.about"),
+								image : item.getImage("organisation.illustration").views.icon.url,
+								link : item.getLink("organisation.website").value.url
+							};
+			},
+			
 		}
-
+	var getContainer = function(){
+		var containerSelector = "#template_container";
+		var container = $(containerSelector);
+		if(container.length === 0){
+			throw "can not find '" + containerSelector + "'";
+		}
+		return container;
+	}
+	//arguments to this function will be used as the predicates for the query
+	global.getResources = function(){
+		predicates = arguments;
+		
 		Helpers.withPrismic(function(ctx) {
-			var containerSelector = "#template_container";
-			if($(containerSelector).length === 0){
-				console.error("can not find '" + containerSelector + "'");
-				return
-			}
+			var container = getContainer();
 			var queryBuilder = ctx.api.form('everything')
 														.ref(ctx.api.master());
 				queryBuilder.query.apply(queryBuilder, predicates)
@@ -43,15 +56,44 @@
 						if(response.total_results_size === 0){
 							console.log("No results returned");
 						}
-							
+												
 						response.results.forEach(function(item){
 							var type = item.type;
 							if(typeof maps[type]  === 'undefined'){ console.log("no map found for " + type);return;}
 							var mappedItem = maps[type](ctx, item);
-							$(containerSelector).loadTemplate("/templates/" + type + ".html", mappedItem, {append:true, isFile:true});
+							container.loadTemplate("/templates/" + type + ".html", mappedItem, {append:true, isFile:true});
 						});
-						//$("#template_holder").loadTemplate("/templates/person.html",people, {append:true, isFile:true});
-        });
+				});
+		});
+	};
+	global.getArticle = function(id, slug){
+		Helpers.withPrismic(function(ctx) {
+			var container = getContainer();
+			ctx.api.form('everything')
+				.ref(ctx.api.master())
+				.query(Prismic.Predicates.at("document.id", id), Prismic.Predicates.at("document.type", "article"))
+				.submit(function(err, docs) {
+					if (err) {
+              console.log(err);
+              done();
+            }
+					var doc = docs.results[0];
+										
+					// If the slug doesn't match
+					if(doc.slug != slug) {
+							// If this is an old valid slug, redirect
+							if(doc.slugs.indexOf(slug) > -1) {
+									document.location = ctx.linkResolver(ctx, doc);
+							} else {
+									document.location = 'notfound.html';
+							}
+					}
+										
+					if(!doc || doc.type !== 'article') {
+							document.location = 'notfound.html';
+					}
+					container.append(doc.asHtml());
+				});
 		});
 	};
 }(window));
